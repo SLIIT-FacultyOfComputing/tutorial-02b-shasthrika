bf273cbf48e881c58506712b3abe2090e6a324d2
shasthri kawya <99796618+IT21165016@users.noreply.github.com>
2022-02-16 12:48:20 +0000

    tutorial 01-rewrite c++ program

--- tute01.cpp
+++ tute01.cpp
@@ -1,3 +1,22 @@
+
+/*Exercise 1 - Calculations
+
+Convert the C program given below which converts a length given in cm to inches to a C++ program.
+
+Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.
+
+e.g.
+
+float data1;
+int data2;
+scanf("%f", &data1); --> std::cin >> data1;
+scanf("%d", &data2); --> std::cin >> data2; 
+You already know that printf() in C is std::cout in C++ e.g.
+
+printf("Hello World") --> std::cout << "Hello World";
+2.54cm = 1 inch
+*/
+
 /*
 #include <stdio.h>
 int main() 
