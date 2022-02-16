1c9605f40efc8337ea0cecd046fe5ad85784b922
shasthri kawya <99796618+IT21165016@users.noreply.github.com>
2022-02-16 13:26:44 +0000

    Tutorial 2_rewrite c++ program

--- tute02.cpp
+++ tute02.cpp
@@ -11,7 +11,7 @@ OtRate = 1500
 Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
-
+/*
 #include <stdio.h>
 int main()
 {
@@ -41,4 +41,29 @@ int main()
    printf("Net Salary is %f ", netSalary);
   
    return 0;
+}*/
+#include<iostream>
+#include<iomanip>
+int main()
+{
+  double salary,netSalary;
+  int etype,otHrs,otRate;
+  std::cout<<"Enter Employe Type:";
+  std::cin>>etype;
+  std::cout<<" Enter Salary:";
+  std::cin>>salary;
+  std::cout<<"Enter OtHrs:";
+  std::cin>>otHrs;
+  switch(etype)
+  {
+    case 1:otRate=1000;
+          break;
+    case 2:otRate=1500;
+          break;
+    default:otRate=1700;
+          break;
+  }
+  netSalary=salary+otHrs*otRate;
+  std::cout<<"Net Salary is "<<netSalary<<std::endl;
+  return 0;
 }
